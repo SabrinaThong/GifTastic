91d0d1b5dc3b7654974d762f06db53d80a378a20
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-05-07 12:06:53 -0500

    adding fixes to the gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,39 +25,38 @@ $("#add-golfer").on("click", function(event) {
     topics.push(newGolfer);
   renderButtons();
 });
-$(document).on("click",".topic", alertGifName)
-$("#buttons").on("click" , function() {
 
-var difGif = $(this).attr("data-name")
+//$(document).on("click",".topic", alertGifName)
+$(document).on("click",".topic",function(){
+    var difGif = $(this).attr("data-name");
+    console.log(difGif)
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + difGif + "&api_key=sCER0NrjS9WlM1txvOrMByVQ1zY7NUu8&limit=10";
+    //console.log(queryURL);
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+        console.log(response)
+        var results = response.data;
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + difGif + "&api_key=sCER0NrjS9WlM1txvOrMByVQ1zY7NUu8&limit=10";
-console.log(queryURL)
-$.ajax({
-   url: queryURL,
-   method: "GET"
-})
+        for (var n=0; n< results.length;n++){
+            var gifDiv = $("<div>");
+            var rating = results[n].rating;
+            var p = $("<p>").text("Rating: " + rating);
+        var personImage = $("<img class='result'>");
+        personImage.attr("src", results[n].images.fixed_height.url);
+            personImage.attr("data-state", "still");
+        personImage.attr("data-still", results[n].images.fixed_height_still.url);
+            personImage.attr("data-animate", results[n].images.fixed_height.url);
 
-.then(function(response) {
-console.log(response)
-var results = response.data;
-for (var n=0; n< results.length;n++){
-    var gifDiv = $("<div>");
-    var rating = results[n].rating;
-    var p = $("<p>").text("Rating: " + rating);
-   var personImage = $("<img class='result'>");
-   personImage.attr("src", results[n].images.fixed_height.url);
-    personImage.attr("data-state", "still");
-   personImage.attr("data-still", results[n].images.fixed_height_still.url);
-    personImage.attr("data-animate", results[n].images.fixed_height.url);
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+            $("#gifshere").prepend(gifDiv);
+        }
+    }) 
+//console.log(queryURL)
 
-    gifDiv.prepend(p);
-    gifDiv.prepend(personImage);
-    $("#gifshere").prepend(gifDiv);
-}
-}) 
-console.log(queryURL)
-
-})
+});
 
 renderButtons();
 $(document).on("click",".result", function() {
